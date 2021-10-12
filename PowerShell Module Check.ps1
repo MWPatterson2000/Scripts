bb3f02cfdd5e5b945239b78cc8eb68393865b6f7
mwpatterson2000 <m.w.patterson@live.com>
2021-10-12 07:58:33 -0400

    Update PowerShell Module Check.ps1

--- PowerShell Module Check.ps1
+++ PowerShell Module Check.ps1
@@ -14,6 +14,7 @@ function Get-Differences
     #Write-Host "File Count:" $count
     #Write-Host "Current:" $latest.FullName[0] # Current Settings
     #Write-Host "Previous:" $latest.FullName[1] # Previous Settings
+    #Pause
 
     If ($count -eq 2) {
         # Read the files in
@@ -26,38 +27,36 @@ function Get-Differences
         $countP = $countP - 1
         for ($numC = 0 ; $numC -le $countC ; $numC++) {
             #$temp = ($checkValuesA[$numC])
-            If (($TempAR).Name -contains ($modulesCurrent[$numC]).Name) {
+            If (($modulesPrevious).Name -contains ($modulesCurrent[$numC]).Name) {
                 for ($numP = 0 ; $numP -le $countP ; $numP++) {
                     If (($modulesPrevious[$numP]).Name -contains ($modulesCurrent[$numC]).Name) {
-                        If ((($modulesPrevious[$numP]).Verions).Major -eq (($modulesCurrent[$numc]).Verions).Major) {
-                            If ((($modulesPrevious[$numP]).Verions).Minor -eq (($modulesCurrent[$numc]).Verions).Minor) {
-                                If ((($modulesPrevious[$numP]).Verions).Build -eq (($modulesCurrent[$numc]).Verions).Build) {
-                                    If ((($modulesPrevious[$numP]).Verions).Revision -eq (($modulesCurrent[$numc]).Verions).Revision) {}
-                                    else {Write-Host "Updated PowerShell Module:" ($modulesCurrent[$numC]).Name}
+                        If ((($modulesPrevious[$numP]).Version).Major -eq (($modulesCurrent[$numC]).Version).Major) {
+                            If ((($modulesPrevious[$numP]).Version).Minor -eq (($modulesCurrent[$numC]).Version).Minor) {
+                                If ((($modulesPrevious[$numP]).Version).Build -eq (($modulesCurrent[$numC]).Version).Build) {
+                                    If ((($modulesPrevious[$numP]).Version).Revision -eq (($modulesCurrent[$numC]).Version).Revision) {}
+                                    else {Write-Host "`tUpdated PowerShell Module:" ($modulesCurrent[$numC]).Name -ForegroundColor Yellow}
                                 }
-                                else {Write-Host "Updated PowerShell Module:" ($modulesCurrent[$numC]).Name}
+                                else {Write-Host "`tUpdated PowerShell Module:" ($modulesCurrent[$numC]).Name -ForegroundColor Yellow}
                             }
-                            else {Write-Host "Updated PowerShell Module:" ($modulesCurrent[$numC]).Name}
+                            else {Write-Host "`tUpdated PowerShell Module:" ($modulesCurrent[$numC]).Name -ForegroundColor Yellow}
                         }
-                        else {Write-Host "Updated PowerShell Module:" ($modulesCurrent[$numC]).Name}
+                        else {Write-Host "`tUpdated PowerShell Module:" ($modulesCurrent[$numC]).Name -ForegroundColor Yellow}
                     }
-                    else {Write-Host "New PowerShell Module:" ($modulesCurrent[$numC]).Name}
                 }
             }
-            else {Write-Host "New PowerShell Module:" ($modulesCurrent[$numC]).Name}
+            else {Write-Host "`tNew PowerShell Module:" ($modulesCurrent[$numC]).Name -ForegroundColor Yellow}
         }
     }
 }
 
+# Get Currnet PowerShell Modules Installed
 $Script:ModuleReportJ = "C:\PowerShell\Installed\$(Get-Date -Format yyyy-MM-dd-HH-mm) - Modules.json"
 $Script:ModuleReportC = "C:\PowerShell\Installed\$(Get-Date -Format yyyy-MM-dd-HH-mm) - Modules.csv"
 $Script:objTemp = New-Object System.Object
 $tempAR = Get-InstalledModule
-#@($tempAR) | ConvertTo-Json | Out-File $Script:ModuleReportJ -Append
 @($tempAR) | Select-Object Version,Name,Repository | ConvertTo-Json | Out-File $Script:ModuleReportJ -Append
 @($tempAR) | Export-Csv -path $Script:ModuleReportC -NoTypeInformation -Encoding UTF8
 
-
+# Compare Currrent & Previous Modules Installed
 Get-Differences
-
 
