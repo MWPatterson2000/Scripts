8cde3a170e91015b375363791b3312f10e74bbf0
mwpatterson2000 <m.w.patterson@live.com>
2021-12-10 11:53:01 -0500

    Create AD Sites to IP's - Multiple Subnets.ps1

---
+++ AD/AD Sites to IP's - Multiple Subnets.ps1
@@ -0,0 +1,39 @@
+﻿<#
+Name: AD Sites to IP's - Multiple Subnets.ps1
+
+This script list out Subnets in each AD Site.
+
+Michael Patterson
+scripts@mwpatterson.com
+
+Revision History
+    2018-07-19 - Initial Release
+
+#>
+
+# Clear Screen
+cls
+
+<#
+# Check For Admin Mode
+#Requires -RunAsAdministrator
+#>
+
+# Set Variables
+
+#<#
+# Get Date & Log Locations
+$date = get-date -Format "yyyy-MM-dd-HH-mm"
+$logRoot = "C:\"
+$logFolder = "Temp\"
+$logFolderPath = $logRoot +$logFolder
+$logFile = "AD Sites to Subnets.csv"
+$logFileName = $date +"-" +$logFile 
+$logPath = $logRoot +$logFolder +$date +"-" +$env:USERDNSDOMAIN +"-" +$logFile
+#>
+
+Get-ADReplicationSubnet -Filter * `
+    | select Name,site `
+    | group site `
+    | select @{Name='Name';Expression={$_.Name.Split(',')[0].Trim('CN=')}},@{Name='Subnets';Expression={$_.Group.Name}} `
+    | Export-Csv $logPath -NoTypeInformation -Encoding UTF8
