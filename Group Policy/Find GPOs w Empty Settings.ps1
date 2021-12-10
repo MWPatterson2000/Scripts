6b4eee06eff20572987685156575a543848dfc7d
mwpatterson2000 <m.w.patterson@live.com>
2021-12-10 11:39:30 -0500

    Create Find GPOs w Empty Settings.ps1

---
+++ Group Policy/Find GPOs w Empty Settings.ps1
@@ -0,0 +1,9 @@
+﻿Get-GPO -all | %{
+$count = $null
+$name = $_.displayname
+$gpo = Get-GPOReport -Name $name -ReportType html
+$count = ([string]$gpo | find  "No settings defined.").count
+if($count -eq 2) {
+[array]$list += $name
+}
+} 
