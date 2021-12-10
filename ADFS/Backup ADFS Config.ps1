a737aa846111f2b305c371c7c75224fb882f66ea
mwpatterson2000 <m.w.patterson@live.com>
2021-12-10 11:29:16 -0500

    Create Backup ADFS Config.ps1

---
+++ ADFS/Backup ADFS Config.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module "C:\Program Files (x86)\ADFS Rapid Recreation Tool\ADFSRapidRecreationTool.dll"
+
+Backup-ADFS -StorageType "FileSystem" -StoragePath "C:\ADFSBackup\" -EncryptionPassword "<Password>" -BackupComment "Backup"
+
+Backup-ADFS -StorageType "FileSystem" -StoragePath "C:\ADFSBackup\" -EncryptionPassword "<Password>" -BackupComment "Backup with DK" -BackupDK
