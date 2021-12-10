64cfc941351bafbcc0e1517556efdb9362f57fbf
mwpatterson2000 <m.w.patterson@live.com>
2021-12-10 11:16:26 -0500

    Create Find Legacy AD Groups.ps1

---
+++ AD/Find Legacy AD Groups.ps1
@@ -0,0 +1,41 @@
+﻿<#
+Name: Find Legacy AD Groups.ps1
+
+This script locates the Legacy AD Groups.
+
+Michael Patterson
+scripts@mwpatterson.com
+
+Revision History
+    2018-05-21 - Initial Release
+
+#>
+
+# Clear Screen
+#cls
+Clear-Host
+
+<#
+# Check For Admin Mode
+#Requires -RunAsAdministrator
+#>
+
+# Set Variables
+
+
+#<#
+# Get Date & Log Locations
+$date = get-date -Format "yyyy-MM-dd-HH-mm"
+$logRoot = "C:\"
+$logFolder = "Temp\"
+#$logFolderPath = $logRoot +$logFolder
+$logFile = "Legacy AD Groups.csv"
+#$logFileName = $date +"-" +$logFile 
+$logPath = $logRoot +$logFolder +$date +"-" +$logFile
+#>
+
+Get-ADGroup -Filter * -Server mfadomain.mfa.net | `
+    Get-ADReplicationAttributeMetadata -Server mfa-dc01.mfadomain.mfa.net -Properties Member -ShowAllLinkedValues | `
+    #Where-Object {$_.Version -eq 0} | Select-Object @{n="Group";e={$_.Object}} -Unique | `
+    Where-Object {$_.Version -eq 0} | Select-Object @{n="LEGACY";e={$_.AttributeValue}},@{n="Group";e={$_.Object}} | `
+    Export-Csv "$logPath" -NoTypeInformation -Encoding UTF8
