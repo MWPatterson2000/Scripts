9f096a64ad944c8c6a94c3e93c22a00344e7be98
mwpatterson2000 <m.w.patterson@live.com>
2021-12-10 11:16:24 -0500

    Create FSMO Role Holders.ps1

---
+++ AD/FSMO Role Holders.ps1
@@ -0,0 +1,21 @@
+﻿function Get-FSMO {
+    param(
+        [Parameter(Mandatory=$True)][string]$forest,
+        [Parameter(Mandatory=$True)][string]$domain
+    )
+
+    $forestInfo = Get-ADForest -Identity $forest | Select-Object SchemaMaster,DomainNamingMaster
+    $domainInfo = Get-ADDomain -Identity $domain | Select-Object PDCEmulator,RIDMaster,InfrastructureMaster
+
+    $fsmo = New-Object -TypeName PSObject -Property @{
+        SchemaMaster = $forestInfo.SchemaMaster
+        DomainNamingMaster = $forestInfo.DomainNamingMaster
+        PDCEmulator = $domainInfo.PDCEmulator
+        RIDMaster = $domainInfo.RIDMaster
+        InfrastructureMaster = $domainInfo.InfrastructureMaster
+    }
+
+    return $fsmo
+}
+
+Get-FSMO -forest <FQDN> -domain <DOMAIN>
