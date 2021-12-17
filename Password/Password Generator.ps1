d0aae621bbba7080cb25ab7cd944955fb070ce8c
mwpatterson2000 <m.w.patterson@live.com>
2021-12-17 10:49:00 -0500

    Create Password Generator.ps1

---
+++ Password/Password Generator.ps1
@@ -0,0 +1,25 @@
+﻿function New-RandomPassword {
+    param(
+        [Parameter()]
+        [int]$MinimumPasswordLength = 5,
+        [Parameter()]
+        [int]$MaximumPasswordLength = 10,
+        [Parameter()]
+        [int]$NumberOfAlphaNumericCharacters = 5,
+        [Parameter()]
+        [switch]$ConvertToSecureString
+    )
+    
+    Add-Type -AssemblyName 'System.Web'
+    $length = Get-Random -Minimum $MinimumPasswordLength -Maximum $MaximumPasswordLength
+    $password = [System.Web.Security.Membership]::GeneratePassword($length,$NumberOfAlphaNumericCharacters)
+    if ($ConvertToSecureString.IsPresent) {
+        ConvertTo-SecureString -String $password -AsPlainText -Force
+    } 
+    else {
+        $password
+    }
+}
+
+
+New-RandomPassword -MinimumPasswordLength 10 -MaximumPasswordLength 15 -NumberOfAlphaNumericCharacters 6 -ConvertToSecureString
