120482dcad4c4b6ca0875b3e37ed2b27d1f8cbf6
mwpatterson2000 <m.w.patterson@live.com>
2022-03-17 13:07:45 -0400

    Update module output

--- PowerShell/Module Cleanup - All Module(s).ps1
+++ PowerShell/Module Cleanup - All Module(s).ps1
@@ -7,17 +7,19 @@ Write-Host "Getting All Versions of PowerShell Modules Installed"
 $Script:ModulesAR = Get-InstalledModule
 
 # Cleanup old versions of PowerShell Modules
-Write-Host "Checking for Old Versions of Modules"
+Write-Host "Checking for Old Version(s) of Modules"
 foreach ($module in $Script:ModulesAR) {
     #Write-Host $module.Name
     $ModuleName = $module.Name
     $count = (Get-Module $ModuleName -ListAvailable).Count
     if ($count -gt 1) {
         $count--
-        Write-Host "Cleaning Up $count Old Version(s) of Module: $ModuleName"
+        Write-Host "`tCleaning Up $count Old Version(s) of Module: $ModuleName" -ForegroundColor Yellow
         $Latest = Get-InstalledModule $ModuleName
         #Get-InstalledModule $ModuleName -AllVersions | ? {$_.Version -ne $Latest.Version} | Uninstall-Module #-WhatIf
         Get-InstalledModule $ModuleName -AllVersions | Where-Object {$_.Version -ne $Latest.Version} | Uninstall-Module -Force -ErrorAction Stop
     }
 }
 
+# End
+Write-Host "Finished Checking for Old Version(s) of Modules"
