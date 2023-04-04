ff638b131030d88a37e8ebeb11dd6d5adf0025de
Mike Patterson <m.w.patterson@live.com>
2023-04-04 16:25:07 -0400

    Get AD Restricted Group Members in AD Forest

---
+++ AD/Get Restricted Groups.ps1
@@ -0,0 +1,166 @@
+<#
+Name: Get Restricted Groups.ps1
+
+This script to get all the Privledged Groups in an AD Forest.
+
+Michael Patterson
+scripts@mwpatterson.com
+
+Revision History
+    2023-04-04 - Initial Release
+
+#>
+
+# Start Function(s)
+function GetGroupInfoForest {
+    [array]$members = Get-ADGroup -Server $forest -Identity $groupID | Get-ADGroupMember -Recursive
+    $groupName = Get-ADGroup -Server $forest -Identity $groupID | Select-Object Name
+    $group = @{}
+    $group | Add-Member -Type NoteProperty -Name 'Domain' -Value $forest
+    $group | Add-Member -Type NoteProperty -Name 'GroupName' -Value $groupName.Name
+    $group | Add-Member -Type NoteProperty -Name 'Members' -Value $(@(Get-ADGroup -Server $forest -Identity $groupID | Get-ADGroupMember).Count)
+    # Add Info to Table
+    [void]$groupCounts.Add($group)
+
+    # Loop each user in the list
+    Foreach ($member in $members) {
+        $user = @{}
+        $userInfo = Get-ADUser -Identity $member -Property * #| Select-Object displayName
+        $lastLogin = [DateTime]::FromFileTime($userInfo.lastLogonTimestamp)
+        $user | Add-Member -Type NoteProperty -Name 'Domain' -Value $forest
+        $user | Add-Member -Type NoteProperty -Name 'Group' -Value $groupName.Name
+        $user | Add-Member -Type NoteProperty -Name 'DisplayName' -Value $userInfo.DisplayName
+        $user | Add-Member -Type NoteProperty -Name 'sAMAccountName' -Value $userInfo.sAMAccountName
+        $user | Add-Member -Type NoteProperty -Name 'userPrincipalName' -Value $userInfo.userPrincipalName
+        $user | Add-Member -Type NoteProperty -Name 'Last Login Timestamp' -Value $lastLogin
+        # Add Info to Table
+        [void]$groupMembers.Add($user)
+    }
+}
+
+function GetGroupInfoDomain {
+    [array]$domainAdmins = Get-ADGroup -Server $domainname -Identity $groupID | Get-ADGroupMember -Recursive
+    $groupName = Get-ADGroup -Server $domainname -Identity $groupID | Select-Object Name
+    $group = @{}
+    $group | Add-Member -Type NoteProperty -Name 'Domain' -Value $domainname
+    $group | Add-Member -Type NoteProperty -Name 'GroupName' -Value $groupName.Name
+    $group | Add-Member -Type NoteProperty -Name 'Members' -Value $(@(Get-ADGroup -Server $domainname -Identity $groupID | Get-ADGroupMember).Count)
+    # Add Info to Table
+    [void]$groupCounts.Add($group)
+    # Loop each user in the list
+    Foreach ($member in $members) {
+        $user = @{}
+        $userInfo = Get-ADUser -Identity $member -Property * #| Select-Object displayName
+        $lastLogin = [DateTime]::FromFileTime($userInfo.lastLogonTimestamp)
+        $user | Add-Member -Type NoteProperty -Name 'Domain' -Value $domainname
+        $user | Add-Member -Type NoteProperty -Name 'Group' -Value $groupName.Name
+        $user | Add-Member -Type NoteProperty -Name 'DisplayName' -Value $userInfo.DisplayName
+        $user | Add-Member -Type NoteProperty -Name 'sAMAccountName' -Value $userInfo.sAMAccountName
+        $user | Add-Member -Type NoteProperty -Name 'userPrincipalName' -Value $userInfo.userPrincipalName
+        $user | Add-Member -Type NoteProperty -Name 'Last Login Timestamp' -Value $lastLogin
+        # Add Info to Table
+        [void]$groupMembers.Add($user)
+    }
+}
+
+# End Function(s)
+
+# Clear Screen
+Clear-Host
+
+# PowerShell 5.x required. The version of PowerShell included with Windows 10
+#Requires -Version 5.0
+
+
+# Create Array
+$groupCounts = [System.Collections.ArrayList]::new()
+$groupMembers = [System.Collections.ArrayList]::new()
+
+# Get Forest Information
+$ForestInfo = Get-ADForest
+$forest = $ForestInfo.RootDomain
+[Array]$allDomains = $ForestInfo.Domains
+
+# Get Date & Log Locations
+$date = get-date -Format "yyyy-MM-dd-HH-mm"
+$logRoot = "C"
+$logFolder = "Temp"
+#$logFolderPath = $logRoot + ":\" + $logFolder + "\"
+$logFile1 = "Group Counts.csv"
+$logFile2 = "Group Members.csv"
+#$logFileName = $date +"-" +$logFile 
+$logPath1 = $logRoot + ":\" + $logFolder + "\" + $date + "-" + $forest + "-" + $logFile1
+$logPath2 = $logRoot + ":\" + $logFolder + "\" + $date + "-" + $forest + "-" + $logFile2
+
+<#
+# Get Domain Name
+$domainName = $env:USERDNSDOMAIN #Full Domain Name 
+#$domainName = $env:USERDOMAIN #Short Domain Name
+
+# Get Domain Details
+#$domaindetails = $null
+$domaindetails = get-addomain $domainName
+
+# Get Domain SID
+$domainSID = $domaindetails.DomainSID
+#>
+
+# Schema Admins
+$groupID = ((Get-ADDomain(Get-ADForest).name).domainSID).value + "-518"
+GetGroupInfoForest
+
+# Enterprise Admins
+$groupID = ((Get-ADDomain(Get-ADForest).name).domainSID).value + "-519"
+GetGroupInfoForest
+
+# Loop each Domain in the list
+$allDomains | ForEach-Object {
+    $domainname = $_
+    
+    # Get Domain Details
+    $domaindetails = $null
+    $domaindetails = get-addomain $domainname
+    $domainSID = $domaindetails.DomainSID
+    
+    # Domain Admins
+    $groupID = ($domainSID).value + "-512"
+    GetGroupInfoDomain
+
+    # Group Policy Creator Owners
+    $groupID = ($domainSID).value + "-520"
+    GetGroupInfoDomain
+
+    # Domain Controllers
+    $groupID = ($domainSID).value + "-516"
+    GetGroupInfoDomain
+
+    # Read-only Domain Controllers
+    $groupID = ($domainSID).value + "-521"
+    GetGroupInfoDomain
+
+    # Cloneable Domain Controllers
+    $groupID = ($domainSID).value + "-522"
+    GetGroupInfoDomain
+
+    # Enterprise Read-only Domain Controllers
+    $groupID = ($domainSID).value + "-498"
+    GetGroupInfoDomain
+
+    # Allowed RODC Password Replication Group
+    $groupID = ($domainSID).value + "-571"
+    GetGroupInfoDomain
+
+    # Denied RODC Password Replication Group
+    $groupID = ($domainSID).value + "-572"
+    GetGroupInfoDomain
+}
+
+<#
+# No of Domain Admins
+$domAdminGroupID = ($domainSID).value + "-512"
+GetGroupInfoDomain
+#>
+
+# Export Data
+$groupCounts | Select-Object Domain, GroupName, Members | Export-Csv -Path $logPath1 -NoTypeInformation -Encoding UTF8
+$groupMembers | Select-Object Domain, Group, DisplayName, sAMAccountName, userPrincipalName, 'Last Login Timestamp' | Export-Csv -Path $logPath2 -NoTypeInformation -Encoding UTF8
