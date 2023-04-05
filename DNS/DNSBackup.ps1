3a0a3273581da8f7080458849d0c82e00123554d
Mike Patterson <m.w.patterson@live.com>
2023-04-05 12:02:06 -0400

    Script Cleanup

--- DNS/DNSBackup.ps1
+++ DNS/DNSBackup.ps1
@@ -1,4 +1,4 @@
-﻿ <#
+﻿<#
 Name: DNSBackup.ps1
 
 This script is Backup DNS Info.
@@ -12,7 +12,7 @@ Revision History
 #>
 
 # Clear Screen
-cls
+Clear-Host
 
 <#
 # Check For Admin Mode
@@ -40,8 +40,8 @@ if ((Test-Path $exportFolderPath) -eq $false) {
     New-Item -Path $exportFolderPath -ItemType directory
     }
 
- # Pulls an environment variable to find the server name, queries it for a list of zones, filters only the primary ones, removes the quotes from the exported .csv file, and saves it to the specified folder.
-Get-DNSServerZone -ComputerName $env:computername | Where-Object{$_.ZoneType -eq "Primary"} | Select-Object ZoneName | ConvertTo-CSV -NoTypeInformation | ForEach-Object {$_ -replace '"', ""} | Out-File "$exportFolderPath\$date-ZoneTemp.csv"
+# Pulls an environment variable to find the server name, queries it for a list of zones, filters only the primary ones, removes the quotes from the exported .csv file, and saves it to the specified folder.
+Get-DNSServerZone -ComputerName $env:computername | Where-Object {$_.ZoneType -eq "Primary"} | Select-Object ZoneName | ConvertTo-CSV -NoTypeInformation | ForEach-Object {$_ -replace '"', ""} | Out-File "$exportFolderPath\$date-ZoneTemp.csv"
 
 # Imports the zone list
 $ZoneList = Get-Content "$exportFolderPath\$date-ZoneTemp.csv"
@@ -54,7 +54,7 @@ ForEach ($line in $ZoneList) {
     # Exports the zone info with the desired naming scheme
     Export-DNSServerZone -Name $line -FileName "$date-${line}.txt"
     # Moves the export file from the default location to the Exports folder
-    move "C:\Windows\System32\dns\$date-${line}.txt" "$exportFolderPath\$date-${line}.txt"
+    Move-Item "C:\Windows\System32\dns\$date-${line}.txt" "$exportFolderPath\$date-${line}.txt"
 }
 # Removes all files in the Exports folder older than 7 days
 #forfiles /p "C:\DNSBackups\Exports" /d -7 /c "cmd /c del @file"
