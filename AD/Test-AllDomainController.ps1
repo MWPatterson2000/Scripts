a1419af9600f29f8be0749b94a84c98547337dbf
Mike Patterson <m.w.patterson@live.com>
2023-04-05 12:02:02 -0400

    Script Cleanup

--- AD/Test-AllDomainController.ps1
+++ AD/Test-AllDomainController.ps1
@@ -1,8 +1,41 @@
+<#
+Name: .ps1
+
+This script is for Testing Connectivity to Domain Controllers.
+
+Michael Patterson
+scripts@mwpatterson.com
+
+Revision History
+    2023-04-05 - Initial Release
+
+#>
+
+# Start Function(s)
 function Test-AllDomainController {
     $dcs=(Get-ADDomainController -Filter *).Name
     foreach ($items in $dcs) {
     Test-Connection $items -Count 1}
     }
 
+# End Function(s)
 
+# Clear Screen
+Clear-Host
+
+<#
+# Self-elevate the script if required
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
+#>
+
+<#
+# Check For Admin Mode
+#Requires -RunAsAdministrator
+#>
+
+# Test Connection to Domain Controllers
 Test-AllDomainController
+
