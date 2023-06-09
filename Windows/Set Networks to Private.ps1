96698abaa24434630421ec4b53308b0137bed137
Mike Patterson <m.w.patterson@live.com>
2023-06-09 08:25:36 -0400

    Upload from personal Repo

---
+++ Windows/Set Networks to Private.ps1
@@ -0,0 +1,9 @@
+﻿## Change NetWorkConnection Category to Private
+#Requires -RunasAdministrator
+
+Get-NetConnectionProfile |
+  Where-Object{ $_.NetWorkCategory -ne 'Private'} |
+  ForEach-Object {
+    $_
+    $_|Set-NetConnectionProfile -NetWorkCategory Private -Confirm
+  }
