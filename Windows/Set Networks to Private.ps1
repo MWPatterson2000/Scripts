2682f3aba4e14bf69ae01568f3c0807e8b78b3c5
Mike Patterson <m.w.patterson@live.com>
2023-06-17 20:05:39 -0400

    Add Header to Script

--- Windows/Set Networks to Private.ps1
+++ Windows/Set Networks to Private.ps1
@@ -1,9 +1,45 @@
-﻿## Change NetWorkConnection Category to Private
-#Requires -RunasAdministrator
+<#
+Name: Set Networks to Private.ps1
 
+This script to Set Networks to Private on Client
+
+Michael Patterson
+scripts@mwpatterson.com
+
+Revision History
+    2023-X-X - Initial Release
+
+#>
+
+# Start Function(s)
+
+# End Function(s)
+
+# Clear Screen
+Clear-Host
+
+#<#
+# PowerShell 5.x required. The version of PowerShell included with Windows 10
+#Requires -Version 5.0
+#>
+
+<#
+# PowerShell Version Requirements - v7.2 (LTS) Min
+#$PSVersionTable
+#$PSVersionTable.PSVersion
+#Requires -Version 7.2
+#>
+
+#<#
+# Check For Admin Mode
+#Requires -RunAsAdministrator
+#>
+
+# Change NetWorkConnection Category to Private
+# Set Each Network Profile to Private
 Get-NetConnectionProfile |
-  Where-Object{ $_.NetWorkCategory -ne 'Private'} |
-  ForEach-Object {
+Where-Object { $_.NetWorkCategory -ne 'Private' } |
+ForEach-Object {
     $_
-    $_|Set-NetConnectionProfile -NetWorkCategory Private -Confirm
-  }
+    $_ | Set-NetConnectionProfile -NetWorkCategory Private -Confirm
+}
