1d97c67d58106ebd883e5a5d3cca2d41b66aa6a1
Mike Patterson <m.w.patterson@live.com>
2023-06-30 15:28:44 -0400

    Update Output

--- AD/Test AD Ports - All DC's.ps1
+++ AD/Test AD Ports - All DC's.ps1
@@ -1,22 +1,25 @@
+# Check for Open AD Ports on all DC's
 
 # Get Domain Controllers
 $DomainName = (Get-ADDomain).DNSRoot
 #$DCList = Get-ADDomainController -Filter * -Server $DomainName | Select-Object Hostname, Site, OperatingSystem
-$DCList = Get-ADDomainController -Filter * -Server $DomainName | Select-Object Hostname, Site, Domain, Forest, Enabled, `
+$DCList = Get-ADDomainController -Filter * -Server $DomainName | Select-Object Hostname, Name, Site, Domain, Forest, Enabled, `
     IsGlobalCatalog, LdapPort, SslPort, IPv4Address, IPv6Address, OperatingSystem
 
-
-$ports | ForEach-Object { Test-NetConnection COMPUTERNAME -port $_ }
+# Check Domain Controllers
 foreach ($DC in $DCList) {
+    Write-Host "Checking Domain Controller:" $DC.Hostname
     #$Ports = "464", "389", "636", "3268", "3269", "53", "88", "49152"
-    $Ports = 53, 88, 137, 139, 389, 445, 464, 636, 3268, 3269
+    #$Ports = 53, 88, 137, 139, 389, 445, 464, 636, 3268, 3269
+    #$Ports = "53", "88", "137", "139", "389", "445", "464", "636", "3268", "3269"
+    $Ports = "53", "88", "139", "389", "445", "464", "636", "3268", "3269"
     Foreach ($Port in $Ports) {
-        $Test = (Test-NetConnection $Target -Port $Port)
+        $Test = (Test-NetConnection $DC.Hostname -Port $Port)
         if ($Test.TcpTestSucceeded -ne $True) {
-            Write-Host “$Target $Port Failed"
+            Write-Host “`t$($DC.Name) $Port Closed - Failed" -ForegroundColor Red
         }
         else {
-            Write-Host “$Target $Port OK"
+            Write-Host “`t$($DC.Name) $Port Open - OK" -ForegroundColor Green
         }
     }
 }
