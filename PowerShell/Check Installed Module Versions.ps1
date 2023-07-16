22e83cc9574ac3104d8901f7d4bb4a5a1da30626
Mike Patterson <m.w.patterson@live.com>
2023-07-15 23:16:18 -0400

    Add Additional Reporting to Scripts

--- PowerShell/Check Installed Module Versions.ps1
+++ PowerShell/Check Installed Module Versions.ps1
@@ -1,25 +1,49 @@
 # Get the Install Module(s) list
-#Get-Installedmodule | Sort-Object Name | Select-Object Name, Version, @{n='Online';e={(Find-module -Name $_.Name).Version}} | Format-Table -Autosize
+#Get-InstalledModule | Sort-Object Name | Select-Object Name, Version, @{n='Online';e={(Find-module -Name $_.Name).Version}} | Format-Table -Autosize
 
-$tempAR = Get-Installedmodule | Select-Object Name, Version, @{ n='Online';e={ (Find-module -Name $_.Name).Version } }
-#$tempAR = Get-Installedmodule | Sort-Object Name | Select-Object Name, Version, @{n='Online';e={(Find-module -Name $_.Name).Version}}
-#$tempAR
+#$Script:ModulesAR = Get-InstalledModule | Select-Object Name, Version, @{ n='Online';e={ (Find-module -Name $_.Name).Version } }
+#$Script:ModulesAR = Get-InstalledModule | Sort-Object Name | Select-Object Name, Version, @{n='Online';e={(Find-module -Name $_.Name).Version}}
+#$Script:ModulesAR
+
+# Get All Versions of PowerShell Modules Installed
+Write-Host "Getting All Versions of PowerShell Module(s) Installed"
+$Script:ModulesAR = Get-InstalledModule | Select-Object Name, Version, @{ n='Online';e={ (Find-module -Name $_.Name).Version } } | Sort-Object Name
+if (-not $Script:ModulesAR) {
+    Write-Host ("`tModules found: 0") -ForegroundColor Yellow
+    return
+}
+else {
+    $ModulesCount = $Script:ModulesAR.Count
+    $DigitsLength = $ModulesCount.ToString().Length
+    Write-Host ("`tModules Found: {0}" -f $ModulesCount) -ForegroundColor Yellow
+}
 
 # Find Updated Module(s)
-$count = @(($tempAR).Name).Count
+Write-Host "Checking for Updated Versions of Modules"
+$i = 0
+$count = @($Script:ModulesAR).Count
 $count = $count - 1
 for ($num = 0 ; $num -le $count ; $num++) {
-    if (($tempAR[$num]).Version -gt ($tempAR[$num]).Online) {
-        #Write-Host "`n" + ($tempAR[$num]).Name: + "Online" -ForegroundColor Red
-        $temp = ($tempAR[$num]).Name + " - Local Newer"
-        Write-Host $temp -ForegroundColor Yellow
-        $tempAR[$num]
-    }
-    if (($tempAR[$num]).Version -lt ($tempAR[$num]).Online) {
-        #Write-Host "`n" + ($tempAR[$num]).Name: + "Local" #-ForegroundColor Yellow
-        $temp = ($tempAR[$num]).Name + " - Online Newer"
+    $i++
+    $Counter = ("[{0,$DigitsLength}/{1,$DigitsLength}]" -f $i, $ModulesCount)
+    $CounterLength = $Counter.Length
+    #Write-Host "Count:" $num
+    #Write-Host "Counter:" $Counter
+    #Write-Host ("{0,$CounterLength} Module Count")
+    if (($Script:ModulesAR[$num]).Version -gt ($Script:ModulesAR[$num]).Online) {
+        #Write-Host "`n" + ($Script:ModulesAR[$num]).Name: + "Online" -ForegroundColor Red
+        #$temp = ($Script:ModulesAR[$num]).Name + " - Local Newer"
         #Write-Host $temp -ForegroundColor Yellow
-        $tempAR[$num]
+        Write-Host ("{0} Local Module Newer {1} - {2}" -f $Counter, ($Script:ModulesAR[$num]).Name, ($Script:ModulesAR[$num]).Version) -ForegroundColor Yellow
+        #Write-Host "$CounterLength Local Module Newer ($Script:ModulesAR[$num]).Name - ($Script:ModulesAR[$num]).Version" -ForegroundColor Yellow
+        $Script:ModulesAR[$num]
+    }
+    if (($Script:ModulesAR[$num]).Version -lt ($Script:ModulesAR[$num]).Online) {
+        #Write-Host "`n" + ($Script:ModulesAR[$num]).Name: + "Local" #-ForegroundColor Yellow
+        #$temp = ($Script:ModulesAR[$num]).Name + " - Online Newer"
+        #Write-Host $temp -ForegroundColor Yellow
+        Write-Host ("{0} Online Module Newer {1} - {2} to {3}" -f $Counter, ($Script:ModulesAR[$num]).Name, ($Script:ModulesAR[$num]).Version, ($Script:ModulesAR[$num]).Online) -ForegroundColor Yellow
+        $Script:ModulesAR[$num]
     }
 }
 
--- PowerShell/Module Cleanup - All Module(s).ps1
+++ PowerShell/Module Cleanup - All Module(s).ps1
@@ -4,15 +4,31 @@
 
 # Copy Modules Folder
 Write-Host "Copy All Versions of PowerShell Module(s) Installed"
-robocopy 'C:\Program Files\WindowsPowerShell\Modules' 'D:\PowerShell\Modules'  /S /R:1 /W:1 /NP /XO /XC /MT:24 /ZB /XF
+$moduleSource = 'C:\Program Files\WindowsPowerShell\Modules'
+$moduleDestination = 'D:\PowerShell\Modules'
+#robocopy 'C:\Program Files\WindowsPowerShell\Modules' 'D:\PowerShell\Modules'  /S /R:1 /W:1 /XO /XC /MT:24 /ZB /XF /NC /NS /NFL /NDL /NP /NJH #/NJS 
+robocopy $moduleSource $moduleDestination  /S /R:1 /W:1 /XO /XC /MT:24 /ZB /XF /NC /NS /NFL /NDL /NP /NJH #/NJS 
 
 # Get All Versions of PowerShell Modules Installed
 Write-Host "Getting All Versions of PowerShell Module(s) Installed"
-$Script:ModulesAR = Get-InstalledModule
+$Script:ModulesAR = Get-InstalledModule | Sort-Object Name
+if (-not $Script:ModulesAR) {
+    Write-Host ("`tModules found: 0") -ForegroundColor Yellow
+    return
+}
+else {
+    $ModulesCount = $Script:ModulesAR.Count
+    $DigitsLength = $ModulesCount.ToString().Length
+    Write-Host ("`tModules Found: {0}" -f $ModulesCount) -ForegroundColor Yellow
+}
 
 # Cleanup old versions of PowerShell Modules
+$i = 0
 Write-Host "Checking for Old Version(s) of Module(s)"
 foreach ($module in $Script:ModulesAR) {
+    $i++
+    $Counter = ("[{0,$DigitsLength}/{1,$DigitsLength}]" -f $i, $ModulesCount)
+    $CounterLength = $Counter.Length
     #Write-Host $module.Name
     $ModuleName = $module.Name
     #$count = (Get-Module $ModuleName -ListAvailable).Count # Faster Option
@@ -21,6 +37,7 @@ foreach ($module in $Script:ModulesAR) {
         if ($count -gt 1) {
             $count--
             Write-Host "`tCleaning Up $count Old Version(s) of Module: $ModuleName" -ForegroundColor Yellow
+            Write-Host ("{0} Uninstalling {1} Previous Version of Module: {2}" -f $Counter, $count, $ModuleName) -ForegroundColor Yellow
             $Latest = Get-InstalledModule $ModuleName
             #Get-InstalledModule $ModuleName -AllVersions | ? {$_.Version -ne $Latest.Version} | Uninstall-Module #-WhatIf
             #Get-InstalledModule $ModuleName -AllVersions | Where-Object {$_.Version -ne $Latest.Version} | Uninstall-Module -Force -ErrorAction Stop
