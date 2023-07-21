9a6050a759706460170222b2ccddc9509cb9bdbc
Mike Patterson <m.w.patterson@live.com>
2023-07-20 21:12:38 -0400

    Test

---
+++ Compress Files & Folders/7zip.ps1
@@ -0,0 +1,15 @@
+
+# Path to 7-Zip
+$7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
+
+# Create Alias
+Set-Alias Compress-7Zip $7ZipPath
+
+# Set Source & Destination
+$Source = "C:\Events\System.txt"
+$Destination = "c:\PS\Events.zip"
+
+# Compress Files/Folder
+Compress-7zip a -mx=9 $Destination $Target
+
+# End
