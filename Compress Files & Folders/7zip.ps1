07b6998e2137ff724c718fcc9c05d4a5bbc57dbd
Mike Patterson <m.w.patterson@live.com>
2023-08-24 14:38:38 -0400

    Update to correct format

--- Compress Files & Folders/7zip.ps1
+++ Compress Files & Folders/7zip.ps1
@@ -11,6 +11,7 @@ $Destination = "c:\PS\Events.zip"
 
 # Compress Files/Folder
 #Compress-7zip a -mx=9 $Destination $Target
-Compress-7zip a -mx9 -r -tzip $Destination $Source
+#Compress-7zip a -mx9 -r -tzip $Destination $Source # Zip Format
+Compress-7zip a -mx9 -r -t7z $Destination $Source # 7Z Format
 
 # End
