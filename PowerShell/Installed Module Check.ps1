60b2ae1878a5c48af22ef7b6b53444d7e9e9ec36
Mike Patterson <m.w.patterson@live.com>
2023-11-19 19:52:22 -0500

    Advanced Module Update

--- PowerShell/Installed Module Check.ps1
+++ PowerShell/Installed Module Check.ps1
@@ -2,67 +2,78 @@
     Allows the User to Check for PowerShell Module Changes on the Client
 #>
 
-# Variables
-#$path = "C:\PowerShell\Installed" # Path to store report and JSON files for comparison
-$path = 'D:\PowerShell\Installed' # Path to store report and JSON files for comparison
+[CmdletBinding()]
+[Alias()]
+[OutputType([int])]
+Param(
+    # Parameter help description
+    #[Parameter(AttributeValues)]
+    #[ParameterType]
+    #$ParameterName
+)
 
-# Start Function(s)
-# Clear Varables
-function Get-UserVariable ($Name = '*') {
-    [CmdletBinding()]
-    #param ()
-    # these variables may exist in certain environments (like ISE, or after use of foreach)
-    $special = 'ps', 'psise', 'psunsupportedconsoleapplications', 'foreach', 'profile'
+Begin {
+    # Variables
+    #$path = "C:\PowerShell\Installed" # Path to store report and JSON files for comparison
+    $path = 'D:\PowerShell\Installed' # Path to store report and JSON files for comparison
 
-    $ps = [PowerShell]::Create()
-    $null = $ps.AddScript('$null=$host;Get-Variable') 
-    $reserved = $ps.Invoke() | 
-    Select-Object -ExpandProperty Name
-    $ps.Runspace.Close()
-    $ps.Dispose()
-    Get-Variable -Scope Global | 
-    Where-Object Name -like $Name |
-    Where-Object { $reserved -notcontains $_.Name } |
-    Where-Object { $special -notcontains $_.Name } |
-    Where-Object Name 
-}
+    # Start Function(s)
+    # Clear Varables
+    function Get-UserVariable ($Name = '*') {
+        [CmdletBinding()]
+        #param ()
+        # these variables may exist in certain environments (like ISE, or after use of foreach)
+        $special = 'ps', 'psise', 'psunsupportedconsoleapplications', 'foreach', 'profile'
 
-# Check for Differences Between Currrent & Previous Modules
-function Get-Differences {
-    [CmdletBinding()]
-    param ()
-    # Get the Files
-    #$dir = "C:\PowerShell\Installed\*- Modules.json"
-    $dir = "$path\*- Modules.json"
-    #Write-Host $dir
-    $latest = @(Get-ChildItem -Path $dir | Sort-Object LastWriteTime -Descending | Select-Object -First 2)
-    $count = @($latest).Count
-    #Write-Host "File Count:" $count
-    #Write-Host "Current:" $latest.FullName[0] # Current Settings
-    #Write-Host "Previous:" $latest.FullName[1] # Previous Settings
+        $ps = [PowerShell]::Create()
+        $null = $ps.AddScript('$null=$host;Get-Variable') 
+        $reserved = $ps.Invoke() | 
+        Select-Object -ExpandProperty Name
+        $ps.Runspace.Close()
+        $ps.Dispose()
+        Get-Variable -Scope Global | 
+        Where-Object Name -like $Name |
+        Where-Object { $reserved -notcontains $_.Name } |
+        Where-Object { $special -notcontains $_.Name } |
+        Where-Object Name 
+    }
 
-    If ($count -eq 2) {
-        # Read the files in
-        $modulesCurrent = Get-Content -Raw -Path $latest.FullName[0] | ConvertFrom-Json # Current Settings
-        $modulesPrevious = Get-Content -Raw -Path $latest.FullName[1] | ConvertFrom-Json # Previous Settings
+    # Check for Differences Between Currrent & Previous Modules
+    function Get-Differences {
+        [CmdletBinding()]
+        param ()
+        # Get the Files
+        #$dir = "C:\PowerShell\Installed\*- Modules.json"
+        $dir = "$path\*- Modules.json"
+        #Write-Host $dir
+        $latest = @(Get-ChildItem -Path $dir | Sort-Object LastWriteTime -Descending | Select-Object -First 2)
+        $count = @($latest).Count
+        #Write-Host "File Count:" $count
+        #Write-Host "Current:" $latest.FullName[0] # Current Settings
+        #Write-Host "Previous:" $latest.FullName[1] # Previous Settings
 
-        $countC = @($modulesCurrent).Count
-        $countC = $countC - 1
-        $countP = @($modulesPrevious).Count
-        $countP = $countP - 1
-        for ($numC = 0 ; $numC -le $countC ; $numC++) {
-            #$temp = ($checkValuesA[$numC])
-            If (($modulesPrevious).Name -contains ($modulesCurrent[$numC]).Name) {
-                #Write-Host "Current:"($modulesCurrent[$numC]).Name (($modulesCurrent[$numC]).Version).Major (($modulesCurrent[$numC]).Version).Minor (($modulesCurrent[$numC]).Version).Build (($modulesCurrent[$numC]).Version).Revision
-                #Write-Host "Current:"$modulesCurrent[$numC]
-                #Write-Host "Previous:"($modulesPrevious).Name
-                for ($numP = 0 ; $numP -le $countP ; $numP++) {
-                    If (($modulesPrevious[$numP]).Name -contains ($modulesCurrent[$numC]).Name) {
-                        #Write-Host "Previous:"($modulesPrevious[$numP]).Name (($modulesPrevious[$numP]).Version).Major (($modulesPrevious[$numP]).Version).Minor (($modulesPrevious[$numP]).Version).Build (($modulesPrevious[$numP]).Version).Revision
-                        #Write-Host "Previous:"$modulesPrevious[$numP]
-                        #$tempM = "Updated," + ($modulesCurrent[$numC]).Name + "," + (($modulesCurrent[$numC]).Version).Major + "." + (($modulesCurrent[$numC]).Version).Minor + "." + (($modulesCurrent[$numC]).Version).Build + "." + (($modulesCurrent[$numC]).Version).Revision
-                        $tempM = 'Updated,' + ($modulesCurrent[$numC]).Name + ',' + ($modulesCurrent[$numC]).Version
-                        <#
+        If ($count -eq 2) {
+            # Read the files in
+            $modulesCurrent = Get-Content -Raw -Path $latest.FullName[0] | ConvertFrom-Json # Current Settings
+            $modulesPrevious = Get-Content -Raw -Path $latest.FullName[1] | ConvertFrom-Json # Previous Settings
+
+            $countC = @($modulesCurrent).Count
+            $countC = $countC - 1
+            $countP = @($modulesPrevious).Count
+            $countP = $countP - 1
+            for ($numC = 0 ; $numC -le $countC ; $numC++) {
+                #$temp = ($checkValuesA[$numC])
+                If (($modulesPrevious).Name -contains ($modulesCurrent[$numC]).Name) {
+                    #Write-Host "Current:"($modulesCurrent[$numC]).Name (($modulesCurrent[$numC]).Version).Major (($modulesCurrent[$numC]).Version).Minor (($modulesCurrent[$numC]).Version).Build (($modulesCurrent[$numC]).Version).Revision
+                    #Write-Host "Current:"$modulesCurrent[$numC]
+                    #Write-Host "Previous:"($modulesPrevious).Name
+                    for ($numP = 0 ; $numP -le $countP ; $numP++) {
+                        If (($modulesPrevious[$numP]).Name -contains ($modulesCurrent[$numC]).Name) {
+                            #Write-Host "Previous:"($modulesPrevious[$numP]).Name (($modulesPrevious[$numP]).Version).Major (($modulesPrevious[$numP]).Version).Minor (($modulesPrevious[$numP]).Version).Build (($modulesPrevious[$numP]).Version).Revision
+                            #Write-Host "Previous:"$modulesPrevious[$numP]
+                            #$tempM = "Updated," + ($modulesCurrent[$numC]).Name + "," + (($modulesCurrent[$numC]).Version).Major + "." + (($modulesCurrent[$numC]).Version).Minor + "." + (($modulesCurrent[$numC]).Version).Build + "." + (($modulesCurrent[$numC]).Version).Revision
+                            $tempM = 'Updated,' + ($modulesCurrent[$numC]).Name + ',' + ($modulesCurrent[$numC]).Version
+                            <#
                         If ((($modulesPrevious[$numP]).Version).Major -eq (($modulesCurrent[$numC]).Version).Major) {
                             If ((($modulesPrevious[$numP]).Version).Minor -eq (($modulesCurrent[$numC]).Version).Minor) {
                                 If ((($modulesPrevious[$numP]).Version).Build -eq (($modulesCurrent[$numC]).Version).Build) {
@@ -86,57 +97,65 @@ function Get-Differences {
                             }
                         }
                         #>
-                        If (($modulesPrevious[$numP]).Version -eq ($modulesCurrent[$numC]).Version) {
-                            #Write-Host "Same"
-                        }
-                        else {
-                            #Write-Host "Current:"$modulesCurrent[$numC]
-                            #Write-Host "Previous:"$modulesPrevious[$numP]
-                            Write-Host "`tUpdated PowerShell Module:" ($modulesCurrent[$numC]).Name -ForegroundColor Yellow
-                            #$tempM = "Updated," + ($modulesCurrent[$numC]).Name + "," + (($modulesCurrent[$numC]).Version).Major + "." + (($modulesCurrent[$numC]).Version).Minor + "." + (($modulesCurrent[$numC]).Version).Build + "." + (($modulesCurrent[$numC]).Version).Revision
-                            $tempM | Out-File $outputCsv -Append
+                            If (($modulesPrevious[$numP]).Version -eq ($modulesCurrent[$numC]).Version) {
+                                #Write-Host "Same"
+                            }
+                            else {
+                                #Write-Host "Current:"$modulesCurrent[$numC]
+                                #Write-Host "Previous:"$modulesPrevious[$numP]
+                                Write-Host "`tUpdated PowerShell Module:" ($modulesCurrent[$numC]).Name -ForegroundColor Yellow
+                                #$tempM = "Updated," + ($modulesCurrent[$numC]).Name + "," + (($modulesCurrent[$numC]).Version).Major + "." + (($modulesCurrent[$numC]).Version).Minor + "." + (($modulesCurrent[$numC]).Version).Build + "." + (($modulesCurrent[$numC]).Version).Revision
+                                $tempM | Out-File $outputCsv -Append
+                            }
                         }
                     }
                 }
+                else {
+                    Write-Host "`tNew PowerShell Module:" ($modulesCurrent[$numC]).Name -ForegroundColor Yellow
+                    $tempM = 'New,' + ($modulesCurrent[$numC]).Name + ',' + (($modulesCurrent[$numC]).Version).Major + '.' + (($modulesCurrent[$numC]).Version).Minor + '.' + (($modulesCurrent[$numC]).Version).Build + '.' + (($modulesCurrent[$numC]).Version).Revision
+                    $tempM | Out-File $outputCsv -Append
+                }
             }
-            else {
-                Write-Host "`tNew PowerShell Module:" ($modulesCurrent[$numC]).Name -ForegroundColor Yellow
-                $tempM = 'New,' + ($modulesCurrent[$numC]).Name + ',' + (($modulesCurrent[$numC]).Version).Major + '.' + (($modulesCurrent[$numC]).Version).Minor + '.' + (($modulesCurrent[$numC]).Version).Build + '.' + (($modulesCurrent[$numC]).Version).Revision
-                $tempM | Out-File $outputCsv -Append
-            }
-        }
-        for ($numP = 0 ; $numP -le $countP ; $numP++) {
-            If (($modulesCurrent).Name -NotContains ($modulesPrevious[$numP]).Name) {
-                Write-Host "`tRemoved PowerShell Module:" ($modulesCurrent[$numP]).Name -ForegroundColor Yellow
-                $tempM = 'Removed,' + ($modulesPrevious[$numP]).Name + ',' + (($modulesPrevious[$numP]).Version).Major + '.' + (($modulesPrevious[$numP]).Version).Minor + '.' + (($modulesCurrent[$numP]).Version).Build + '.' + (($modulesCurrent[$numP]).Version).Revision
-                $tempM | Out-File $outputCsv -Append
+            for ($numP = 0 ; $numP -le $countP ; $numP++) {
+                If (($modulesCurrent).Name -NotContains ($modulesPrevious[$numP]).Name) {
+                    Write-Host "`tRemoved PowerShell Module:" ($modulesCurrent[$numP]).Name -ForegroundColor Yellow
+                    $tempM = 'Removed,' + ($modulesPrevious[$numP]).Name + ',' + (($modulesPrevious[$numP]).Version).Major + '.' + (($modulesPrevious[$numP]).Version).Minor + '.' + (($modulesCurrent[$numP]).Version).Build + '.' + (($modulesCurrent[$numP]).Version).Revision
+                    $tempM | Out-File $outputCsv -Append
+                }
             }
         }
     }
+
+    # End Function(s)
+
 }
 
-# End Function(s)
+Process {
+    # Get Currnet PowerShell Modules Installed
+    $Script:ModuleReportJ = "$path\$(Get-Date -Format yyyy-MM-dd-HH-mm) - Modules.json"
+    $Script:ModuleReportC = "$path\$(Get-Date -Format yyyy-MM-dd-HH-mm) - Modules.csv"
+    $Script:ModuleReportS = "$path\$(Get-Date -Format yyyy-MM-dd-HH-mm) - Modules Names.csv"
+    $Script:objTemp = New-Object System.Object
+    #Get-InstalledModule | Select-Object Name,Repository,Version | Format-Table -AutoSize
+    $tempAR = Get-InstalledModule | Sort-Object Name
+    @($tempAR) | Export-Csv -path $Script:ModuleReportC -NoTypeInformation -Encoding UTF8
+    #@($tempAR) | Select-Object Version,Name,Repository | ConvertTo-Json | Out-File $Script:ModuleReportJ -Append
+    @($tempAR) | Select-Object Name, Repository, Version | ConvertTo-Json | Out-File $Script:ModuleReportJ -Append
+    @($tempAR) | Select-Object Name, Repository, Version | Export-Csv -path $Script:ModuleReportS -NoTypeInformation -Encoding UTF8
 
-# Get Currnet PowerShell Modules Installed
-$Script:ModuleReportJ = "$path\$(Get-Date -Format yyyy-MM-dd-HH-mm) - Modules.json"
-$Script:ModuleReportC = "$path\$(Get-Date -Format yyyy-MM-dd-HH-mm) - Modules.csv"
-$Script:ModuleReportS = "$path\$(Get-Date -Format yyyy-MM-dd-HH-mm) - Modules Names.csv"
-$Script:objTemp = New-Object System.Object
-#Get-InstalledModule | Select-Object Name,Repository,Version | Format-Table -AutoSize
-$tempAR = Get-InstalledModule | Sort-Object Name
-@($tempAR) | Export-Csv -path $Script:ModuleReportC -NoTypeInformation -Encoding UTF8
-#@($tempAR) | Select-Object Version,Name,Repository | ConvertTo-Json | Out-File $Script:ModuleReportJ -Append
-@($tempAR) | Select-Object Name, Repository, Version | ConvertTo-Json | Out-File $Script:ModuleReportJ -Append
-@($tempAR) | Select-Object Name, Repository, Version | Export-Csv -path $Script:ModuleReportS -NoTypeInformation -Encoding UTF8
+    # Write Changes to File
+    $outputCsv = "$path\$(Get-Date -Format yyyy-MM-dd-HH-mm) - Update Modules.csv"
 
-# Write Changes to File
-$outputCsv = "$path\$(Get-Date -Format yyyy-MM-dd-HH-mm) - Update Modules.csv"
+    # Compare Currrent & Previous Modules Installed
+    Get-Differences
 
-# Compare Currrent & Previous Modules Installed
-Get-Differences
+}
 
-# Clear Variables
-Write-Host "`nScript Cleanup"
-Get-UserVariable | Remove-Variable -ErrorAction SilentlyContinue
+End {
+    # Clear Variables
+    Write-Host "`nScript Cleanup"
+    Get-UserVariable | Remove-Variable -ErrorAction SilentlyContinue
 
-# End
+    # End
+    Exit
+}
--- PowerShell/Module Maintenance.ps1
+++ PowerShell/Module Maintenance.ps1
@@ -2,36 +2,82 @@
     Allows the User to Update & Cleanup Old Versions of PowerShell Module(s) on the Client
 #>
 
-# Copy Modules Folder
-Write-Host "Copy All Versions of PowerShell Module(s) Installed"
-robocopy 'C:\Program Files\WindowsPowerShell\Modules' 'C:\PowerShell\Modules'  /S /R:1 /W:1 /NP /XO /XC /MT:24 /ZB /XF
+[CmdletBinding()]
+[Alias()]
+[OutputType([int])]
+Param(
+    # Parameter help description
+    #[Parameter(AttributeValues)]
+    #[ParameterType]
+    #$ParameterName
+)
 
-# Update Modules
-Write-Host "Updating All Versions of PowerShell Module(s) Installed"
-Update-Module
+Begin {
+    # Start Function(s)
+    # Clear Varables
+    function Get-UserVariable ($Name = '*') {
+        [CmdletBinding()]
+        #param ()
+        # these variables may exist in certain environments (like ISE, or after use of foreach)
+        $special = 'ps', 'psise', 'psunsupportedconsoleapplications', 'foreach', 'profile'
 
-# Get All Versions of PowerShell Modules Installed
-Write-Host "Getting All Versions of PowerShell Module(s) Installed"
-$Script:ModulesAR = Get-InstalledModule
-
-# Cleanup old versions of PowerShell Modules
-Write-Host "Checking for Old Version(s) of Modules"
-foreach ($module in $Script:ModulesAR) {
-    #Write-Host $module.Name
-    $ModuleName = $module.Name
-    #$count = (Get-Module $ModuleName -ListAvailable).Count # Faster Option
-    $count = (Get-InstalledModule $ModuleName -AllVersions).Count # Slower Option
-    if ($ModuleName -ne "Pester") {
-        if ($count -gt 1) {
-            $count--
-            Write-Host "`tCleaning Up $count Old Version(s) of Module: $ModuleName" -ForegroundColor Yellow
-            $Latest = Get-InstalledModule $ModuleName
-            #Get-InstalledModule $ModuleName -AllVersions | ? {$_.Version -ne $Latest.Version} | Uninstall-Module #-WhatIf
-            Get-InstalledModule $ModuleName -AllVersions | Where-Object {$_.Version -ne $Latest.Version} | Uninstall-Module -Force -ErrorAction Stop
-        }
+        $ps = [PowerShell]::Create()
+        $null = $ps.AddScript('$null=$host;Get-Variable') 
+        $reserved = $ps.Invoke() | 
+        Select-Object -ExpandProperty Name
+        $ps.Runspace.Close()
+        $ps.Dispose()
+        Get-Variable -Scope Global | 
+        Where-Object Name -like $Name |
+        Where-Object { $reserved -notcontains $_.Name } |
+        Where-Object { $special -notcontains $_.Name } |
+        Where-Object Name 
     }
-    else {Write-Host "`tSkipping Cleaning Up Old Version(s) of Module: $ModuleName" -ForegroundColor Yellow}
+
+    # End Function(s)
 }
 
-# End Cleanup
-Write-Host "Finished Checking for Old Version(s) of Modules"
+Process {
+    # Copy Modules Folder
+    Write-Host 'Copy All Versions of PowerShell Module(s) Installed'
+    robocopy 'C:\Program Files\WindowsPowerShell\Modules' 'C:\PowerShell\Modules'  /S /R:1 /W:1 /NP /XO /XC /MT:24 /ZB /XF
+
+    # Update Modules
+    Write-Host 'Updating All Versions of PowerShell Module(s) Installed'
+    Update-Module
+
+    # Get All Versions of PowerShell Modules Installed
+    Write-Host 'Getting All Versions of PowerShell Module(s) Installed'
+    $Script:ModulesAR = Get-InstalledModule
+
+    # Cleanup old versions of PowerShell Modules
+    Write-Host 'Checking for Old Version(s) of Modules'
+    foreach ($module in $Script:ModulesAR) {
+        #Write-Host $module.Name
+        $ModuleName = $module.Name
+        #$count = (Get-Module $ModuleName -ListAvailable).Count # Faster Option
+        $count = (Get-InstalledModule $ModuleName -AllVersions).Count # Slower Option
+        if ($ModuleName -ne 'Pester') {
+            if ($count -gt 1) {
+                $count--
+                Write-Host "`tCleaning Up $count Old Version(s) of Module: $ModuleName" -ForegroundColor Yellow
+                $Latest = Get-InstalledModule $ModuleName
+                #Get-InstalledModule $ModuleName -AllVersions | ? {$_.Version -ne $Latest.Version} | Uninstall-Module #-WhatIf
+                Get-InstalledModule $ModuleName -AllVersions | Where-Object { $_.Version -ne $Latest.Version } | Uninstall-Module -Force -ErrorAction Stop
+            }
+        }
+        else { Write-Host "`tSkipping Cleaning Up Old Version(s) of Module: $ModuleName" -ForegroundColor Yellow }
+    }
+}
+
+End {
+    # End Cleanup
+    Write-Host 'Finished Checking for Old Version(s) of Modules'
+
+    # Clear Variables
+    Write-Host "`nScript Cleanup"
+    Get-UserVariable | Remove-Variable -ErrorAction SilentlyContinue
+
+    # End
+    Exit
+}
