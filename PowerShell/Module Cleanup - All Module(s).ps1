2f09343393bf988e61d96b77cddacb9b5d712858
Mike Patterson <m.w.patterson@live.com>
2023-12-02 17:55:21 -0500

    Added Progress Bar

--- PowerShell/Module Cleanup - All Module(s).ps1
+++ PowerShell/Module Cleanup - All Module(s).ps1
@@ -1,6 +1,18 @@
 <#
-    Allows the User to Cleanup Old Versions of PowerShell Module(s) on the Client
+Name: Module Cleanup - All Module(s).ps1
+
+Allows the User to Cleanup Old Versions of PowerShell Module(s) on the Client
+
+Michael Patterson
+scripts@mwpatterson.com
+
+Revision History
+    2021-10-21 - Initial Release
+    2023-11-19 - Converted to Advanced Script
+    2023-12-02 - Added Progress Bar
+
 #>
+
 [CmdletBinding()]
 [Alias()]
 [OutputType([int])]
@@ -45,25 +57,47 @@ Process {
     robocopy $moduleSource $moduleDestination  /S /R:1 /W:1 /XO /XC /MT:24 /ZB /XF /NC /NS /NFL /NDL /NP /NJH #/NJS 
 
     # Get All Versions of PowerShell Modules Installed
-    Write-Host 'Getting All Versions of PowerShell Module(s) Installed'
-    $Script:ModulesAR = Get-InstalledModule | Sort-Object Name
+    #Write-Host "Getting All Versions of PowerShell Module(s) Installed"
+    Write-Host "Getting Count of PowerShell Module(s) Installed - $(Get-Date)"
+    $Script:ModulesAR = Get-InstalledModule | Select-Object * | Sort-Object Name
+
+    # Build Variables
+    #$Script:ModulesAR
+    #Write-Host "`tModule(s) Found:" ($Script:ModulesAR).Count
+    #$Script:ModulesCount = $Script:ModulesAR.Count
+    #Write-Host "`tModule(s) Found:" $Script:ModulesCount
+    $Script:counter1 = 0
+
     if (-not $Script:ModulesAR) {
         Write-Host ("`tModules found: 0") -ForegroundColor Yellow
+        # Clear Variables
+        Write-Host "`nScript Cleanup"
+        Get-UserVariable | Remove-Variable -ErrorAction SilentlyContinue
+        # End
+        #Exit
         return
     }
     else {
-        $ModulesCount = $Script:ModulesAR.Count
-        $DigitsLength = $ModulesCount.ToString().Length
-        Write-Host ("`tModules Found: {0}" -f $ModulesCount) -ForegroundColor Yellow
+        $Script:ModulesCount = $Script:ModulesAR.Count
+        Write-Host ("`tModules Found: {0}" -f $Script:ModulesCount) -ForegroundColor Yellow
     }
 
     # Cleanup old versions of PowerShell Modules
-    $i = 0
+    #$i = 0
     Write-Host 'Checking for Old Version(s) of Module(s)'
     foreach ($module in $Script:ModulesAR) {
-        $i++
-        $Counter = ("[{0,$DigitsLength}/{1,$DigitsLength}]" -f $i, $ModulesCount)
-        $CounterLength = $Counter.Length
+        # Build Progress Bar
+        $Script:counter1++
+        $Script:percentComplete1 = ($Script:counter1 / $Script:ModulesCount) * 100
+        If ($Script:percentComplete1 -lt 1) {
+            $Script:percentComplete1 = 1
+        }
+        Write-Progress -Id 1 -Activity 'Checking Module' -Status "Module # $Script:counter1 of $Script:ModulesCount" -PercentComplete $Script:percentComplete1
+        #Write-Progress -Id 1 -Activity 'Checking Module' -Status "Module # $Script:counter1" -PercentComplete $Script:percentComplete1 -CurrentOperation "Module $($module.Name)"
+
+        #$i++
+        #$Counter = ("[{0,$DigitsLength}/{1,$DigitsLength}]" -f $i, $Script:ModulesCount)
+        #$CounterLength = $Counter.Length
         #Write-Host $module.Name
         $ModuleName = $module.Name
         #$count = (Get-Module $ModuleName -ListAvailable).Count # Faster Option
@@ -82,6 +116,7 @@ Process {
         else { Write-Host "`tSkipping Cleaning Up Old Version(s) of Module: $ModuleName" -ForegroundColor Yellow }
     }
 
+    Write-Progress -Id 1 -Activity 'Checking Module' -Status "Module # $Script:counter1 of $Script:ModulesCount" -Completed
 }
 
 End {
