2ea77e5408a755a9d609606cff3ce7cf9ee182e6
Mike Patterson <m.w.patterson@live.com>
2024-03-14 23:34:33 -0400

    Moved Scripts

---
+++ Sample/Sleep.ps1
@@ -0,0 +1,18 @@
+﻿$StartDate=(GET-DATE)
+Start-Sleep -Seconds 62
+$EndDate=(GET-DATE)
+NEW-TIMESPAN –Start $StartDate –End $EndDate | Select-Object Minutes, Seconds
+#([timespan]NEW-TIMESPAN –Start $StartDate –End $EndDate).TotalSeconds
+
+
+#$Duration=$EndDate-$StartDate
+#Write-host $Duration
+#Write-host $Duration
+
+
+$stopwatch = New-Object System.Diagnostics.Stopwatch
+$stopwatch.Start()
+Start-Sleep -Seconds 62
+$stopwatch.Stop()
+$stopwatch
+
